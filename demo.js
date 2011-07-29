64eeb23276b67c9fb14f89263f52524afb540eb4
cage <517020375@qq.com>
2011-07-29 13:09:57 +0800

    first commit

---
+++ demo.js
@@ -0,0 +1,172 @@
+﻿window.onload = init_all;
+
+function init_all(){
+	//获取canvas上下文
+	var canvas = document.getElementById("canvas");
+	if(!canvas.getContext) return;
+	var ctx = canvas.getContext("2d");
+	
+	//获取图片列表
+	var imgList = document.getElementById("pic_list").getElementsByTagName("img");
+	
+	//获取初始尺寸
+	var box = document.getElementById("canvas_box");
+	
+	var record = {};
+	for(var i = 0; i < imgList.length; i++){
+		record["img" + i] = {
+			"trueWidth" : imgList[i].width,
+			"trueHeight" : imgList[i].height
+		}
+	}
+	
+	
+	
+	//重置图片尺寸
+	function rSize(){
+		for(var i = 0; i < imgList.length; i++){
+			var trueHeight = imgList[i].height;
+			if(record["img" + i]["trueHeight"] >= box.clientHeight){
+			imgList[i].height = Math.round((box.clientHeight)*10)/10;
+			imgList[i].width = Math.round((imgList[i].width*box.clientHeight/trueHeight)*10)/10;}
+			else{
+				imgList[i].height = record["img" + i]["trueHeight"];
+				imgList[i].width = record["img" + i]["trueWidth"];
+			}
+		}
+	}
+	rSize();
+	
+	//定义计数器、操作类型、翻转角度
+	count.num = 0;
+	count.type = "";
+	count.radius = 0;
+	count.curImg = imgList[count.num];
+	
+	function count(){
+	
+		//根据操作类型改变计数器和翻转角度的值
+		switch(count.type){
+			case "next" :
+			if(count.num < imgList.length-1) return count.num++;
+			else return count.num = 0;
+			break;
+			
+			case "prev" :
+			if(count.num <= 0) return count.num = imgList.length-1;
+			else return count.num--;
+			break;
+			
+			case "rotate_left" :
+			if(count.radius < 3) return count.radius++;
+			else return count.radius = 0;
+			break;
+			
+			case "rotate_right" :
+			if(count.radius >= 3) return count.radius = 0;
+			else return count.radius--;
+			break;
+		}
+	}
+	
+	//显示初始图
+	showImg(imgList[count.num]);
+	
+	
+	//窗口大小改变重绘图像
+	window.onresize = function(){
+		draw();
+	}
+	
+	
+	//下一张
+	document.getElementById("next").onclick = function(){
+		page(this.id);
+	};
+	
+	
+	//上一张
+	document.getElementById("prev").onclick = function(){
+		page(this.id);
+	};
+	
+	
+	//左旋转
+	document.getElementById("rotate_left").onclick = function(){
+		draw(this.id);
+	};
+	
+	
+	
+	//右旋转
+	document.getElementById("rotate_right").onclick = function(){
+		draw(this.id);
+	};
+	
+	
+	//翻页函数
+	function page(type){
+		rSize();
+		count.type = type;
+		count.radius = 0;
+		count();
+		count.curImg = imgList[count.num];
+		showImg(count.curImg);
+	}
+	
+	
+	
+	//旋转函数
+	function draw(type){
+		count.type = type;
+		count();
+		console.log(count.radius);
+		if(count.radius%2 != 0){
+			var trueWidth = count.curImg.width;
+			if(record["img" + count.num]["trueWidth"] >= box.clientHeight){
+				count.curImg.width = box.clientHeight;
+				count.curImg.height = count.curImg.height*box.clientHeight/trueWidth;}
+			else if(record["img" + count.num]["trueHeight"] <= box.clientHeight){
+				count.curImg.width = record["img" + count.num]["trueHeight"];
+				count.curImg.height = record["img" + count.num]["trueWidth"];
+			}
+			canvas.setAttribute("width",count.curImg.height);
+			canvas.setAttribute("height",count.curImg.width);
+			canvasPosit();
+			ctx.translate(count.curImg.height/2,count.curImg.width/2);
+			rotate();
+		}
+		else{
+			rSize();
+			canvas.setAttribute("width",count.curImg.width);
+			canvas.setAttribute("height",count.curImg.height);
+			canvasPosit();
+			ctx.translate(count.curImg.width/2,count.curImg.height/2);
+			rotate();
+		}
+	}
+	
+	
+	//旋转绘制
+	function rotate(){
+		ctx.save();
+		ctx.rotate(-count.radius*Math.PI/2);
+		ctx.drawImage(count.curImg,-count.curImg.width/2,-count.curImg.height/2,count.curImg.width,count.curImg.height);
+		ctx.restore();
+	}
+	
+	//绘制图像
+	function showImg(obj){
+		canvas.setAttribute("width",obj.width);
+		canvas.setAttribute("height",obj.height);
+		canvasPosit();
+		ctx.drawImage(obj,0,0,obj.width,obj.height);
+	}
+	
+	//图像垂直居中
+	function canvasPosit(){
+		var mTop = (box.clientHeight - canvas.clientHeight)/2 + "px";
+		canvas.style.top = mTop;
+	}
+	
+}
---
+++ demo2.js
@@ -0,0 +1,204 @@
+﻿/* 获取索引 */
+function getIndex(cur,obj){
+	for(var i = 0;i < obj.length; i++){
+		if(obj[i] == cur) return i;
+	}
+}
+
+/* 计数器、翻转角度 */
+count.index = 0;
+count.type = "";
+count.radius = 0;
+function count(type){
+	var imgList = document.getElementById("pic_list").getElementsByTagName("img");
+	switch(count.type){
+		case "next" :
+		if(count.index < imgList.length-1) return count.index++;
+		else return count.index = 0;
+		break;
+		
+		case "prev" :
+		if(count.index <= 0) return count.index = imgList.length-1;
+		else return count.index--;
+		break;
+		
+		case "rotate_left" :
+		if(count.radius < 3) return count.radius++;
+		else return count.radius = 0;
+		break;
+		
+		case "rotate_right" :
+		if(count.radius > 3 ) return count.radius = 0;
+		else return count.radius--;
+		break;
+	}
+}
+
+/* 显示图片 */
+function showImg(box,canvas,img,canvasObj,src){
+	canvasObj.clearRect(0,0,document.documentElement.clientWidth,document.documentElement.clientHeight);
+	img.onload = function(){
+		canvasPosit(box,canvas);
+		if(this.height >= box.clientHeight){
+			this.showHeight = box.clientHeight;
+			this.showWidth = Math.round(this.width*box.clientHeight/this.height);
+		}
+		else{
+			this.showHeight = this.height;
+			this.showWidth = this.width;
+		}
+		canvas.setAttribute("width",this.showWidth);
+		canvas.setAttribute("height",this.showHeight);
+		canvasPosit(box,canvas);
+		canvasObj.drawImage(this,0,0,this.showWidth,this.showHeight);
+	}
+	img.src = "";
+	img.src = src;
+}
+
+/* 垂直居中设置 */
+function canvasPosit(box,canvas){
+	var wrapHeight = window.innerHeight;
+	var barHeight = document.getElementById("bar").clientHeight + 6;
+	box.style.height = parseInt((wrapHeight - barHeight) * 0.98) + "px";
+	box.style.top = parseInt((wrapHeight - barHeight) * 0.01) + "px";
+	canvas.height < box.clientHeight ? canvas.style.top = (box.clientHeight - canvas.clientHeight)/2 + "px" : canvas.style.top = 0;
+}
+
+
+/* 查看大图 */
+function viewImg(){
+	var myCanvas = document.getElementById("myCanvas");
+	if(!myCanvas.getContext) return;
+	var ctx = myCanvas.getContext("2d");
+	var canvasBox = document.getElementById("canvas_box");
+	var oImgList = document.getElementById("pic_list").getElementsByTagName("img");
+	var img = document.getElementById("view_img");
+	var btn = document.getElementById("bar").getElementsByTagName("a");
+	
+	
+	/* 事件方法 */
+	var operate = {
+		page : function(obj){
+			count.type = obj.id;
+			count();
+			showImg(canvasBox,myCanvas,img,ctx,oImgList[count.index].src);
+			count.radius = 0;
+		},
+		rotate : function(oBtn,oImg){
+			count.type = oBtn.id;
+			count();
+			canvasPosit(canvasBox,myCanvas);
+			var x = 0;
+			var y = 0;
+			if(count.radius % 2 != 0){
+				if(oImg.width >= canvasBox.clientHeight){
+					oImg.showWidth = canvasBox.clientHeight;
+					oImg.showHeight = oImg.height*oImg.showWidth/oImg.width;
+				}
+				else{
+					oImg.showWidth = oImg.width;
+					oImg.showHeight = oImg.height;
+				}
+				x = oImg.showHeight;
+				y = oImg.showWidth;
+			}
+			else{
+				if(oImg.height >= canvasBox.clientHeight){
+					oImg.showHeight = canvasBox.clientHeight;
+					oImg.showWidth = Math.round(oImg.width*canvasBox.clientHeight/oImg.height);
+				}
+				else{
+					oImg.showHeight = oImg.height;
+					oImg.showWidth = oImg.width;
+				}
+				x = oImg.showWidth;
+				y = oImg.showHeight;
+			}
+			myCanvas.setAttribute("width",x);
+			myCanvas.setAttribute("height",y);
+			ctx.translate(x/2,y/2);
+			canvasPosit(canvasBox,myCanvas);
+			ctx.save();
+			ctx.rotate(-count.radius*Math.PI/2);
+			ctx.drawImage(oImg,-oImg.showWidth/2,-oImg.showHeight/2,oImg.showWidth,oImg.showHeight);
+			ctx.restore();
+			ctx.translate(-x/2,-y/2);
+			if(document.getElementById("cover")){
+			var cover = document.getElementById("cover");
+				cover.style.width = document.documentElement.clientWidth + "px";
+				cover.style.height = document.documentElement.clientHeight + "px";
+			}
+			console.log(count.radius);
+		},
+		close : function(obj){
+			count.index = 0;
+			count.radius = 0;
+			document.getElementsByTagName("html")[0].style.overflow = "auto";
+			document.getElementById("bar").style.display = "none";
+			canvasBox.style.display = "none";
+			document.getElementById("cover").style.display = "none";
+		},
+		zoom : function(obj){
+			alert("zoom");
+		},
+	}
+	
+	myCanvas.onmousemove = function(e){
+		var px = e.clientX;
+		var py = e.clientY;
+		var a = document.documentElement.clientWidth;
+		var b = myCanvas.width;
+		if(px >= (a - b)/2 && px < a/2){
+			this.style.cursor = "url(http://s3.chenchao.cctvcjw.com/statics/images/shenghuo/left.cur), pointer";
+			this.onclick = function(){
+				operate["page"](document.getElementById("prev"),img);
+			}
+		}
+		else if(px > a/2 && px <= (a+b)/2){
+			this.style.cursor = "url(http://s3.chenchao.cctvcjw.com/statics/images/shenghuo/right.cur), pointer";
+			this.onclick =function(){
+				operate["page"](document.getElementById("next"),img);
+			}
+		}
+	}
+	
+	/* 跟随窗口大小变化 */
+	window.onresize = function(){
+		operate["rotate"]('',img);
+	}
+	
+	
+	/* 触发显示大图 */
+	for(var i = 0; i < oImgList.length; i++){
+		oImgList[i].onclick = function(){
+			count.index = getIndex(this,oImgList);
+			count.radius = 0;
+			document.getElementsByTagName("html")[0].style.overflow = "hidden";
+			document.getElementById("bar").style.display = "block";
+			canvasBox.style.display = "block";
+			showImg(canvasBox,myCanvas,img,ctx,this.src);
+			if(!document.getElementById("cover")){
+				var cover = document.createElement("div");
+				cover.setAttribute("id","cover");
+				var style = 
+					"width : "+document.documentElement.clientWidth +"px;height:"+document.documentElement.clientHeight +"px;position :absolute;top : 0px;left : 0px;zIndex : 888;opacity : 0.3;background:#30f;display:block"
+				cover.setAttribute("style",style)
+				document.body.appendChild(cover);
+			}
+			else{
+				document.getElementById("cover").style.display = "block";
+			}
+		}
+	}
+	
+	/* 绑定事件 */
+	for(var i = 0; i < btn.length; i++){
+		btn[i].onclick = function(){
+			var oType = this.getAttribute("class");
+			operate[oType](this,img);
+		}
+	}
+}
+
+window.onload = viewImg;
